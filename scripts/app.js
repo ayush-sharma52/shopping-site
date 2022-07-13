03e066dfb850e2d49b6f59f325e723aa33b2fcb6
ayush-sharma52 <ayush052sharma@gmail.com>
2022-07-14 03:32:12 +0530

    adding html dynamically

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ import { canvas } from "./UI/reCAPTCHA.js";
 const cartButton=document.getElementById('cart-btn');
 const backdrop=document.getElementById('backdrop');
 const cartModal=document.getElementById('cart-modal');
-const itemsList=document.querySelectorAll('#items .card');//selecting all items
+const itemsListNode=document.querySelector('#items'); 
 const cartItemsList=cartModal.querySelector('ul');
 const totalCost=cartModal.querySelector('#cost');
 const aTags=document.querySelectorAll('a');
@@ -14,6 +14,19 @@ const submitBtn=document.querySelector('#captcha-modal span')
 aTags.forEach((a)=>{
     a.setAttribute("draggable","false");
 })
+const itemsList=["jackets1.jfif Jackets ₹386",
+"jackets2.jfif jackets ₹337",
+"download.jfif Jeans ₹130",
+"shirtsmen2.jfif Shirt ₹290",
+"jeans1.jfif Jeans ₹130",
+"shoes3.jfif Shoes ₹235",
+"jeans3.jfif Jeans ₹165",
+"shoes1.jfif Shoes ₹190.80",
+"tshirtsmen1.jfif T-shirts ₹70",
+"jackets4.jfif Jackets ₹380.40",
+"tshirtsmen3.jfif T-shirts ₹130",
+"shoes2.jfif Shoes ₹325"]
+
 function toggleCaptcha(){
 backdrop.classList.add('invisible');
 captchaModal.classList.add('invisible');
@@ -31,28 +44,28 @@ let total=0;
 function updateTotal(){
     total=total.toFixed(2);
     total=parseFloat(total);
-    totalCost.textContent=`$${total}`;
+    totalCost.textContent=`₹${total}`;
 }
 
 
-cartItemsList.addEventListener('dragenter', event => {
+cartModal.addEventListener('dragenter', event => {
     if (event.dataTransfer.types[0] === 'text/plain') {
       cartItemsList.parentElement.classList.add('droppable');
       event.preventDefault();
     }
-  });
-  cartItemsList.addEventListener('dragover', event => {
+});
+cartModal.addEventListener('dragover', event => {
     if (event.dataTransfer.types[0] === 'text/plain') {
       event.preventDefault();
     }
- });
+});
  
- cartItemsList.addEventListener('dragleave', event => {
-    if (event.relatedTarget.closest('ul') !== cartItemsList) {  //for defining thw whole area inside cartmodal list
+cartModal.addEventListener('dragleave', event => {
+    if (event.relatedTarget.closest('div').id !== cartModal.id ) {  //for defining thw whole area inside cartmodal list
     cartItemsList.parentElement.classList.remove('droppable');
-  }});
+}});
 
-  cartItemsList.addEventListener('drop', event => {
+cartModal.addEventListener('drop', event => {
     const prjId = event.dataTransfer.getData('text/plain');
     console.log(prjId);
     availableItems.forEach(item=>{
@@ -60,21 +73,38 @@ cartItemsList.addEventListener('dragenter', event => {
        item.addToCarthandler();
    
     })
-    cartItemsList.parentElement.classList.remove('droppable');
+    cartModal.classList.remove('droppable');
     // event.preventDefault(); // not required
-  });
+});
 
 class storeItem{
-    constructor(itemNode,id){
-      const addBtn=itemNode.querySelector('button');
+    constructor(itemString,id){
+    this.itemNode=this.render(itemString);
+    const addBtn=this.itemNode.querySelector('button');
     addBtn.addEventListener('click',this.addToCarthandler); //adding event listeners to each item to click and add to cart
     this.id=id;
-    this.item=itemNode;
     this.connectDrag();
     }
+    render(string){
+   let arr=string.split(' ');
+   let location=arr[0];
+   let name=arr[1];
+   let price=arr[2];
+   const itemDiv =document.createElement('div');
+   itemDiv.classList.add('col-xs-4');
+   itemDiv.innerHTML=`
+   <div class="card" >
+      <img class="card-img-top" src="./images/${location}" alt="Card image" >
+    <div class="card-body">
+      <h4 class="card-title">${name}<span>${price}</span><button style="font-size:24px">Add <i class="fa fa-shopping-cart"></i></button></h4>
+    </div>
+     </div>`
+  itemsListNode.append(itemDiv);
+  return itemDiv;
+  }
     deleteBtnhandler(event){
         event.target.closest('li').remove();
-        const elementPrice=parseFloat(event.target.closest('li').querySelector('p').textContent.split('$')[1]);
+        const elementPrice=parseFloat(event.target.closest('li').querySelector('p').textContent.split('₹')[1]);
         total-=elementPrice
         updateTotal();
         }
@@ -83,12 +113,12 @@ class storeItem{
             newItem.classList.add("list-group-item");
             newItem.innerHTML=` <span class="badge">delete</span> 
             <h4 class="list-group-item-heading">${name}</h4>
-            <p class="list-group-item-text">$${cost}</p>`
+            <p class="list-group-item-text">₹${cost}</p>`
             cartItemsList.prepend(newItem);
             return newItem;
         } 
     addToCarthandler=()=>{   //prepending every item we clicked to the cart-modal
-        const h4HTML=this.item.querySelector('h4').textContent.split('$');
+        const h4HTML=this.itemNode.querySelector('h4').textContent.split('₹');
         const name=h4HTML[0]
         const cost=h4HTML[1].split('A')[0];
         total+=parseFloat(cost);
@@ -98,17 +128,19 @@ class storeItem{
         deleteBtn.addEventListener('click',this.deleteBtnhandler);
     }
     connectDrag(){
-       this.item.addEventListener('dragstart',event => {
+       this.itemNode.addEventListener('dragstart',event => {
         event.dataTransfer.setData('text/plain',this.id);})
-        this.item.addEventListener('dragend',event=>{    
+        this.itemNode.addEventListener('dragend',event=>{    
         console.log(event);})
     }
 }
+function renderItems(itemsArray){
 let id=0; 
-itemsList.forEach((i)=>{ 
+itemsArray.forEach((i)=>{ 
     availableItems.push(new storeItem(i,id));
     ++id;
 })
+}
 
 function clearAllbtnhandler(){
   cartItemsList.innerHTML='';
@@ -117,15 +149,14 @@ function clearAllbtnhandler(){
 }
 const mp=new map({lat: 28.639232, lng: 77.2046848} ); ///giving locatiion coordinates of the shop
 const obj=new canvas('canvas');
-
 cartButton.addEventListener('click',cartButtonhandler);
 clearAllbtn.addEventListener('click',clearAllbtnhandler);
 submitBtn.addEventListener('click',()=>{
 if(obj.drawn)
 toggleCaptcha();
 else
-alert('bosdike draw krle pehle')
+alert('please first draw something')
 
 })
 
-
+renderItems(itemsList);
